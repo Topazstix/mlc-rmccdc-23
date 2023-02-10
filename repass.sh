757526e55c7fa39f0c57a55eb2d9e7fb704ddd9d
Topazstix <r.drone@protonmail.ch>
2023-02-10 10:14:01 -0700

    Change repass to operate on all systems

--- repass.sh
+++ repass.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/bash
+#!/bin/bash
 grep -wE '(\/bin\/sh|bash|ash|zsh|fish)' /etc/passwd | cut -d ':' -f 1 > userlist.txt
 
 for i in `more userlist.txt `;do
-    echo $i:new_password | chpasswd
+    echo -e "new_password\nnew_password" | passwd $i
 done
